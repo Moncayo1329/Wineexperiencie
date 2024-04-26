f0008729ffb7ac0217d6964d7170442d6d950f91
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-04-26 21:19:53 +0100

    first commit

---
+++ src/Pages/Future.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+function Future(){
+return(
+<div></div>
+
+)
+
+
+}
+
+
+
+export{Future}
---
+++ src/Pages/Inicio.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Inicio(){
+return(
+<div>
+    <h1>Welcome to Wine experiencia</h1>
+    <p>I am just an wine enthusiast</p> 
+    <h2>You can see every wine that open every week and my future boghts </h2>
+
+</div>
+
+
+)
+
+
+
+
+}
+
+
+export {Inicio}
